a646659cdee554ee78b01c5cd0905b9fdb13a62d
Yuvir SHARMA <ysharma@c4r18s3.wethinkcode.co.za>
2019-06-11 11:13:01 +0200

    libft

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,11 +6,11 @@
 /*   By: ysharma <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 07:07:26 by ysharma           #+#    #+#             */
-/*   Updated: 2019/06/10 22:10:37 by root             ###   ########.fr       */
+/*   Updated: 2019/06/11 11:12:35 by ysharma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
+#include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -20,45 +20,62 @@ char	**ft_strsplit(char const *s, char c)
 	int		i;
 	int		len;
 	int 	count;
+	int 	j;
 	
 	count = 0;
+	j = 0;
 	i = 0;
-	len = 0;
+	len = 1;
+	if (!s || !c)
+		return (NULL);
+	while (s[i] == c)
+	{
+		count++;
+		i++;
+	}
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
+			count++;
 			if (s[i + 1] != c && s[i + 1] != '\0')
-			len++;
+				len++;
 		}
 		i++;
 	}
-	if (!(str = (char **)malloc(len + 1)))
+	if (!(str = (char **)malloc(sizeof(char *) * (len + 1))))
 		return (NULL);
+	if ((count == 0 && len == 1) || len == 1)
+		return (NULL);
+	while (len > count)
+	{
+		str[count] = ft_strnew(i + 1);
+		count++;
+	}
 	i = 0;
-
+	count = 0;
+	while (s[i] == c)
+		i++;
 	while (s[i] != '\0')
 	{
-		if (s[i] != c)
-				str[count][i] = s[i];
-		else 
-			if (s[i + 1] != c && s[i + 1] != '\0')
+		if (len > 0)
+		{
+			 if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
 			{
-				count++;
+				i++;
 				len--;
+				count++;
 			}
-			i++;
+			else if (s[i] == c)
+				i++;
+			else 
+			{
+				str[count][j] = s[i];
+				i++;
+				j++;
+			}
+		}
 	}
 	return (str);
 }
 
-int	main(int ac, char **av)
-{
-	char	**str;
-
-	if (ac == 3)
-	{
-		str = ft_strsplit(av[1], av[2][0]);
-		printf("%c\n%c",*str[0], *str[1]);
-	}
-}
